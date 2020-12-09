9988539bd81592a5629874b6dcc83de2584f5c30
fy8303 <fy_8303@sina.com>
2020-12-09 10:47:00 +0800

    Update TrustAllCerts.java

--- app/src/main/java/com/example/httprequest/TrustAllCerts.java
+++ app/src/main/java/com/example/httprequest/TrustAllCerts.java
@@ -1,14 +1,17 @@
 package com.example.httprequest;
 
+import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 /**
@@ -38,9 +41,30 @@ public class TrustAllCerts implements X509TrustManager {
             sslContext.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
             sslSocketFactory = sslContext.getSocketFactory();
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
         return sslSocketFactory;
+    }
+
+    public static X509TrustManager createTrustManager() {
+        X509TrustManager trustManager = null;
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+
+            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                throw new IllegalStateException("Unexpected default trust managers: "
+                        + Arrays.toString(trustManagers));
+            }
+
+            trustManager = (X509TrustManager) trustManagers[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return trustManager;
     }
 
     public static class TrustAllHostnameVerifier implements HostnameVerifier {
